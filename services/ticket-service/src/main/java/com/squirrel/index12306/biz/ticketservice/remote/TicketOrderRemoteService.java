df9001639e50826a89d3b99ca40bad33c32f08e1
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-12-11 09:57:24 +0800

    补充knife4j注释

--- services/ticket-service/src/main/java/com/squirrel/index12306/biz/ticketservice/remote/TicketOrderRemoteService.java
+++ services/ticket-service/src/main/java/com/squirrel/index12306/biz/ticketservice/remote/TicketOrderRemoteService.java
@@ -2,6 +2,8 @@ package com.squirrel.index12306.biz.ticketservice.remote;
 
 import com.squirrel.index12306.biz.ticketservice.remote.dto.TicketOrderCreateRemoteReqDTO;
 import com.squirrel.index12306.framework.starter.convention.result.Result;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
  * 车票订单远程服务调用
  */
 @FeignClient(value = "index12306-order-service",url = "")
+@Tag(name = "车票订单远程服务调用")
 public interface TicketOrderRemoteService {
 
     /**
@@ -18,6 +21,7 @@ public interface TicketOrderRemoteService {
      * @param requestParam 创建车票订单请求参数
      * @return 订单号
      */
+    @Operation(summary = "创建车票订单")
     @PostMapping("/api/order-service/order/ticket/create")
     Result<String> createTicketOrder(@RequestBody TicketOrderCreateRemoteReqDTO requestParam);
 }
