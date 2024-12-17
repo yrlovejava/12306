49b54c1a5be1fe05accb32ad242fad1a42f9102f
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-12-17 12:13:28 +0800

    聚合层引入支付服务相关配置

--- services/aggregation-service/src/main/java/com/squirrel/index12306/biz/aggregationservice/AggregationServiceApplication.java
+++ services/aggregation-service/src/main/java/com/squirrel/index12306/biz/aggregationservice/AggregationServiceApplication.java
@@ -3,6 +3,7 @@ package com.squirrel.index12306.biz.aggregationservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 12306 聚合服务应用启动器
@@ -10,13 +11,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {
         "com.squirrel.index12306.biz.userservice",
         "com.squirrel.index12306.biz.ticketservice",
-        "com.squirrel.index12306.biz.orderservice"
+        "com.squirrel.index12306.biz.orderservice",
+        "com.squirrel.index12306.biz.payservice"
 })
 @MapperScan(value = {
         "com.squirrel.index12306.biz.userservice.dao.mapper",
         "com.squirrel.index12306.biz.ticketservice.dao.mapper",
-        "com.squirrel.index12306.biz.orderservice.dao.mapper"
+        "com.squirrel.index12306.biz.orderservice.dao.mapper",
+        "com.squirrel.index12306.biz.payservice.dao.mapper"
 })
+@EnableFeignClients("com.squirrel.index12306.biz.ticketservice.remote")
 public class AggregationServiceApplication {
 
     public static void main(String[] args) {
