c3a18cec0e7c900f0afb461f8f7ffedf0abe8917
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-12-18 21:21:45 +0800

    完善重置车座接口

--- services/ticket-service/src/main/java/com/squirrel/index12306/biz/ticketservice/controller/TempSeatController.java
+++ services/ticket-service/src/main/java/com/squirrel/index12306/biz/ticketservice/controller/TempSeatController.java
@@ -1,14 +1,27 @@
 package com.squirrel.index12306.biz.ticketservice.controller;
 
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.squirrel.index12306.biz.ticketservice.common.enums.SeatStatusEnum;
+import com.squirrel.index12306.biz.ticketservice.dao.entity.CarriageDO;
 import com.squirrel.index12306.biz.ticketservice.dao.entity.SeatDO;
+import com.squirrel.index12306.biz.ticketservice.dao.entity.TrainStationRelationDO;
+import com.squirrel.index12306.biz.ticketservice.dao.mapper.CarriageMapper;
 import com.squirrel.index12306.biz.ticketservice.dao.mapper.SeatMapper;
+import com.squirrel.index12306.biz.ticketservice.dao.mapper.TrainStationRelationMapper;
+import com.squirrel.index12306.framework.starter.cache.DistributedCache;
 import com.squirrel.index12306.framework.starter.convention.result.Result;
 import com.squirrel.index12306.framework.starter.web.Results;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static com.squirrel.index12306.biz.ticketservice.common.constant.RedisKeyConstant.TRAIN_STATION_REMAINING_TICKET;
 
 /**
  * TODO 待删除，联调临时解决方案
@@ -19,6 +32,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TempSeatController {
 
     private final SeatMapper seatMapper;
+    private final TrainStationRelationMapper trainStationRelationMapper;
+    private final CarriageMapper carriageMapper;
+    private final DistributedCache distributedCache;
 
     /**
      * 座位重置
@@ -27,8 +43,33 @@ public class TempSeatController {
     public Result<Void> purchaseTickets(@RequestParam String trainId) {
         SeatDO seatDO = new SeatDO();
         seatDO.setTrainId(Long.parseLong(trainId));
-        seatDO.setSeatStatus(0);
+        seatDO.setSeatStatus(SeatStatusEnum.AVAILABLE.getCode());
         seatMapper.update(seatDO, Wrappers.lambdaUpdate());
+        // 查询列车站点关系
+        List<TrainStationRelationDO> trainStationRelationDOList = trainStationRelationMapper.selectList(Wrappers.lambdaQuery(TrainStationRelationDO.class)
+                .eq(TrainStationRelationDO::getTrainId, trainId));
+        // 查询不同车厢类型
+        List<CarriageDO> carriageDOList = carriageMapper.selectList(Wrappers.lambdaQuery(CarriageDO.class)
+                .eq(CarriageDO::getTrainId, trainId)
+                .groupBy(CarriageDO::getCarriageType)
+                .select(CarriageDO::getCarriageType)
+        );
+        // 恢复redis中的各种车厢类型的余票
+        StringRedisTemplate stringRedisTemplate = (StringRedisTemplate) distributedCache.getInstance();
+        for (TrainStationRelationDO each : trainStationRelationDOList) {
+            // 获取key后缀
+            String keySuffix = StrUtil.join("_",each.getTrainId(),each.getDeparture(),each.getArrival());
+            for (CarriageDO item : carriageDOList) {
+                QueryWrapper<CarriageDO> wrapper = new QueryWrapper<>();
+                wrapper.select("sum(seat_count) as seatCount");
+                wrapper.eq("carriage_type",item.getCarriageType());
+                wrapper.eq("train_id",trainId);
+                CarriageDO carriageDO = carriageMapper.selectOne(wrapper);
+                stringRedisTemplate.opsForHash().put(TRAIN_STATION_REMAINING_TICKET + keySuffix,
+                        String.valueOf(item.getCarriageType()),
+                        String.valueOf(carriageDO.getSeatCount()));
+            }
+        }
         return Results.success();
     }
 }
