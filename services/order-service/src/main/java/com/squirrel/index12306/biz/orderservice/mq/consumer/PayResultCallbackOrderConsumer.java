b6ffd9a7dbd961b2bed9087967e049352ed59cef
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2025-01-01 22:25:11 +0800

    开发支付结果回调订单消费者幂等处理

--- services/order-service/src/main/java/com/squirrel/index12306/biz/orderservice/mq/consumer/PayResultCallbackOrderConsumer.java
+++ services/order-service/src/main/java/com/squirrel/index12306/biz/orderservice/mq/consumer/PayResultCallbackOrderConsumer.java
@@ -6,6 +6,9 @@ import com.squirrel.index12306.biz.orderservice.dto.domain.OrderStatusReversalDT
 import com.squirrel.index12306.biz.orderservice.mq.domain.MessageWrapper;
 import com.squirrel.index12306.biz.orderservice.mq.event.PayResultCallbackOrderEvent;
 import com.squirrel.index12306.biz.orderservice.service.OrderService;
+import com.squirrel.index12306.framework.starter.idempotent.annotation.Idempotent;
+import com.squirrel.index12306.framework.starter.idempotent.enums.IdempotentSceneEnum;
+import com.squirrel.index12306.framework.starter.idempotent.enums.IdempotentTypeEnum;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
@@ -28,6 +31,13 @@ public class PayResultCallbackOrderConsumer implements RocketMQListener<MessageW
 
     private final OrderService orderService;
 
+    @Idempotent(
+            uniqueKeyPrefix = "index12306-order:pay_result_callback:",// 唯一键的前缀
+            key = "#message.getKeys()+'_'+#message.hashCode()",// 生成key的SPEL表达式
+            type = IdempotentTypeEnum.SPEL,// 表达式类型
+            scene = IdempotentSceneEnum.MQ,// 幂等场景，MQ
+            keyTimeout = 7200L// key有效时间
+    )
     @Transactional(rollbackFor = Exception.class)
     @Override
     public void onMessage(MessageWrapper<PayResultCallbackOrderEvent> message) {
