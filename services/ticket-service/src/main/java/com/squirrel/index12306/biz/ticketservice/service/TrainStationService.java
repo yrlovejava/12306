bb34e9da732b64491ddf0b7263fb5e5c7ab2e45c
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2025-01-11 14:10:53 +0800

    开发计算列车站点路线关系

--- services/ticket-service/src/main/java/com/squirrel/index12306/biz/ticketservice/service/TrainStationService.java
+++ services/ticket-service/src/main/java/com/squirrel/index12306/biz/ticketservice/service/TrainStationService.java
@@ -1,5 +1,6 @@
 package com.squirrel.index12306.biz.ticketservice.service;
 
+import com.squirrel.index12306.biz.ticketservice.dto.domain.RouteDTO;
 import com.squirrel.index12306.biz.ticketservice.dto.resp.TrainStationQueryRespDTO;
 
 import java.util.List;
@@ -16,4 +17,15 @@ public interface TrainStationService {
      * @return 列车经停站信息
      */
     List<TrainStationQueryRespDTO> listTrainStationQuery(String trainId);
+
+    /**
+     * 计算列车站点路线关系
+     * 获取开始站点和目的站点及中间站点信息
+     *
+     * @param trainId   列车 ID
+     * @param departure 出发站
+     * @param arrival   到达站
+     * @return 列车站点路线关系信息
+     */
+    List<RouteDTO> listTrainStationRoute(String trainId, String departure, String arrival);
 }
--- services/ticket-service/src/main/java/com/squirrel/index12306/biz/ticketservice/service/impl/TrainStationServiceImpl.java
+++ services/ticket-service/src/main/java/com/squirrel/index12306/biz/ticketservice/service/impl/TrainStationServiceImpl.java
@@ -1,15 +1,19 @@
 package com.squirrel.index12306.biz.ticketservice.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.squirrel.index12306.biz.ticketservice.dao.entity.TrainStationDO;
 import com.squirrel.index12306.biz.ticketservice.dao.mapper.TrainStationMapper;
+import com.squirrel.index12306.biz.ticketservice.dto.domain.RouteDTO;
 import com.squirrel.index12306.biz.ticketservice.dto.resp.TrainStationQueryRespDTO;
 import com.squirrel.index12306.biz.ticketservice.service.TrainStationService;
+import com.squirrel.index12306.biz.ticketservice.toolkit.StationCalculateUtil;
 import com.squirrel.index12306.framework.starter.common.toolkit.BeanUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 列车站点接口实现层
@@ -31,5 +35,26 @@ public class TrainStationServiceImpl implements TrainStationService {
         List<TrainStationDO> trainStationDOList = trainStationMapper.selectList(Wrappers.lambdaQuery(TrainStationDO.class)
                 .eq(TrainStationDO::getTrainId, trainId));
         return BeanUtil.convert(trainStationDOList,TrainStationQueryRespDTO.class);
+    }
+
+    /**
+     * 计算列车站点路线关系
+     * 获取开始站点和目的站点及中间站点信息
+     *
+     * @param trainId   列车 ID
+     * @param departure 出发站
+     * @param arrival   到达站
+     * @return 列车站点路线关系信息
+     */
+    @Override
+    public List<RouteDTO> listTrainStationRoute(String trainId, String departure, String arrival) {
+        // 查询所有站点
+        List<TrainStationDO> trainStationDOList = trainStationMapper.selectList(Wrappers.lambdaQuery(TrainStationDO.class)
+                .eq(TrainStationDO::getTrainId, trainId));
+        // 解析站点信息
+        List<String> trainStationAllList = trainStationDOList.stream()
+                .map(TrainStationDO::getDeparture)
+                .toList();
+        return StationCalculateUtil.throughStation(trainStationAllList,departure,arrival);
     }
 }
