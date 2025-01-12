2ce8a83b4d66bfffebc3adfdc7121a4b2953dc5e
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2025-01-12 21:05:44 +0800

    扩展远程调用移除构造函数

--- frameworks/convention/src/main/java/com/squirrel/index12306/framework/starter/convention/exception/RemoteException.java
+++ frameworks/convention/src/main/java/com/squirrel/index12306/framework/starter/convention/exception/RemoteException.java
@@ -1,11 +1,16 @@
 package com.squirrel.index12306.framework.starter.convention.exception;
 
+import com.squirrel.index12306.framework.starter.convention.errorcode.BaseErrorCode;
 import com.squirrel.index12306.framework.starter.convention.errorcode.IErrorCode;
 
 /**
  * 远程服务调用异常
  */
 public class RemoteException extends AbstractException {
+
+    public RemoteException(String message) {
+        this(message, null, BaseErrorCode.REMOTE_ERROR);
+    }
 
     public RemoteException(String message, IErrorCode errorCode) {
         this(message, null, errorCode);
