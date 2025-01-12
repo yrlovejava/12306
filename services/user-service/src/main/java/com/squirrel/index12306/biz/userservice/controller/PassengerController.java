1765b31a909961f983dad4d5c377b71de352e6d3
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2025-01-12 20:32:33 +0800

    乘车人管理支持 HTTP 接口防重复提交

--- services/user-service/src/main/java/com/squirrel/index12306/biz/userservice/controller/PassengerController.java
+++ services/user-service/src/main/java/com/squirrel/index12306/biz/userservice/controller/PassengerController.java
@@ -6,7 +6,11 @@ import com.squirrel.index12306.biz.userservice.dto.resp.PassengerActualRespDTO;
 import com.squirrel.index12306.biz.userservice.dto.resp.PassengerRespDTO;
 import com.squirrel.index12306.biz.userservice.service.PassengerService;
 import com.squirrel.index12306.framework.starter.convention.result.Result;
+import com.squirrel.index12306.framework.starter.idempotent.annotation.Idempotent;
+import com.squirrel.index12306.framework.starter.idempotent.enums.IdempotentSceneEnum;
+import com.squirrel.index12306.framework.starter.idempotent.enums.IdempotentTypeEnum;
 import com.squirrel.index12306.framework.starter.web.Results;
+import com.squirrel.index12306.frameworks.starter.user.core.UserContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,15 +27,25 @@ public class PassengerController {
 
     /**
      * 根据用户名查询乘车人列表
+     * @return Result<List<PassengerRespDTO>> 乘车人列表
      */
     @GetMapping("/api/user-service/passenger/query")
-    public Result<List<PassengerRespDTO>> listPassengerQueryByUsername(String username) {
-        return Results.success(passengerService.listPassengerQueryByUsername(username));
+    public Result<List<PassengerRespDTO>> listPassengerQueryByUsername() {
+        return Results.success(passengerService.listPassengerQueryByUsername(UserContext.getUsername()));
     }
 
     /**
      * 新增乘车人
+     * @param requestParam 新增乘车人的信息
+     * @return Result<Void>
      */
+    @Idempotent(
+            uniqueKeyPrefix = "index12306-user:lock_passenger-alter:",
+            key = "T(com.squirrel.index12306.frameworks.starter.user.core.UserContext).getUsername()",
+            type = IdempotentTypeEnum.SPEL,
+            scene = IdempotentSceneEnum.RESTAPI,
+            message = "正在新增乘车人，请稍后再试"
+    )
     @PostMapping("/api/user-service/passenger/save")
     public Result<Void> savePassenger(@RequestBody PassengerReqDTO requestParam) {
         passengerService.savePassenger(requestParam);
@@ -40,7 +54,16 @@ public class PassengerController {
 
     /**
      * 修改乘车人
+     * @param requestParam 修改的乘车人信息
+     * @return Result<Void>
      */
+    @Idempotent(
+            uniqueKeyPrefix = "index12306-user:lock_passenger-alter:",
+            key = "T(com.squirrel.index12306.frameworks.starter.user.core.UserContext).getUsername()",
+            type = IdempotentTypeEnum.SPEL,
+            scene = IdempotentSceneEnum.RESTAPI,
+            message = "正在修改乘车人，请稍后再试..."
+    )
     @PostMapping("/api/user-service/passenger/update")
     public Result<Void> updatePassenger(@RequestBody PassengerReqDTO requestParam) {
         passengerService.updatePassenger(requestParam);
@@ -60,7 +83,17 @@ public class PassengerController {
 
     /**
      * 移除乘车人
+     * TODO: post路径错误 remote -> remove
+     * @param requestParam 移除的乘车人信息
+     * @return Result<Void>
      */
+    @Idempotent(
+            uniqueKeyPrefix = "index12306-user:lock_passenger-alter:",
+            key = "T(com.squirrel.index12306.frameworks.starter.user.core.UserContext).getUsername()",
+            type = IdempotentTypeEnum.SPEL,
+            scene = IdempotentSceneEnum.RESTAPI,
+            message = "正在移除乘车人，请稍后再试..."
+    )
     @PostMapping("/api/user-service/passenger/remote")
     public Result<Void> removePassenger(@RequestBody PassengerRemoveReqDTO requestParam) {
         passengerService.removePassenger(requestParam);
