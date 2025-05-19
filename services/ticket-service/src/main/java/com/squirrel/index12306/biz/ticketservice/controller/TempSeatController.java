fbfe29f6bb9752a0f1aa3c0ae4861fe4976628ff
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2025-05-19 15:42:48 +0800

    修复座位重置数据错误问题

--- services/ticket-service/src/main/java/com/squirrel/index12306/biz/ticketservice/controller/TempSeatController.java
+++ services/ticket-service/src/main/java/com/squirrel/index12306/biz/ticketservice/controller/TempSeatController.java
@@ -1,13 +1,10 @@
 package com.squirrel.index12306.biz.ticketservice.controller;
 
 import cn.hutool.core.util.StrUtil;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.squirrel.index12306.biz.ticketservice.common.enums.SeatStatusEnum;
-import com.squirrel.index12306.biz.ticketservice.dao.entity.CarriageDO;
 import com.squirrel.index12306.biz.ticketservice.dao.entity.SeatDO;
 import com.squirrel.index12306.biz.ticketservice.dao.entity.TrainStationRelationDO;
-import com.squirrel.index12306.biz.ticketservice.dao.mapper.CarriageMapper;
 import com.squirrel.index12306.biz.ticketservice.dao.mapper.SeatMapper;
 import com.squirrel.index12306.biz.ticketservice.dao.mapper.TrainStationRelationMapper;
 import com.squirrel.index12306.framework.starter.cache.DistributedCache;
@@ -35,7 +32,6 @@ public class TempSeatController {
 
     private final SeatMapper seatMapper;
     private final TrainStationRelationMapper trainStationRelationMapper;
-    private final CarriageMapper carriageMapper;
     private final DistributedCache distributedCache;
 
     /**
@@ -46,32 +42,17 @@ public class TempSeatController {
         SeatDO seatDO = new SeatDO();
         seatDO.setTrainId(Long.parseLong(trainId));
         seatDO.setSeatStatus(SeatStatusEnum.AVAILABLE.getCode());
-        seatMapper.update(seatDO, Wrappers.lambdaUpdate());
+        seatMapper.update(seatDO, Wrappers.lambdaUpdate(SeatDO.class).eq(SeatDO::getTrainId, trainId));
         ThreadUtil.sleep(5000);
         StringRedisTemplate stringRedisTemplate = (StringRedisTemplate) distributedCache.getInstance();
         // 查询列车站点关系
         List<TrainStationRelationDO> trainStationRelationDOList = trainStationRelationMapper.selectList(Wrappers.lambdaQuery(TrainStationRelationDO.class)
                 .eq(TrainStationRelationDO::getTrainId, trainId));
-        // 查询不同车厢类型
-        List<CarriageDO> carriageDOList = carriageMapper.selectList(Wrappers.lambdaQuery(CarriageDO.class)
-                .eq(CarriageDO::getTrainId, trainId)
-                .groupBy(CarriageDO::getCarriageType)
-                .select(CarriageDO::getCarriageType)
-        );
-        // 恢复redis中的各种车厢类型的余票
+        // 删除redis中的各种车厢类型的余票
         for (TrainStationRelationDO each : trainStationRelationDOList) {
             // 获取key后缀
-            String keySuffix = StrUtil.join("_",each.getTrainId(),each.getDeparture(),each.getArrival());
-            for (CarriageDO item : carriageDOList) {
-                QueryWrapper<CarriageDO> wrapper = new QueryWrapper<>();
-                wrapper.select("sum(seat_count) as seatCount");
-                wrapper.eq("carriage_type",item.getCarriageType());
-                wrapper.eq("train_id",trainId);
-                CarriageDO carriageDO = carriageMapper.selectOne(wrapper);
-                stringRedisTemplate.opsForHash().put(TRAIN_STATION_REMAINING_TICKET + keySuffix,
-                        String.valueOf(item.getCarriageType()),
-                        String.valueOf(carriageDO.getSeatCount()));
-            }
+            String keySuffix = StrUtil.join("_", each.getTrainId(), each.getDeparture(), each.getArrival());
+            stringRedisTemplate.delete(TRAIN_STATION_REMAINING_TICKET + keySuffix);
         }
         stringRedisTemplate.delete(TICKET_AVAILABILITY_TOKEN_BUCKET + trainId);
         return Results.success();
