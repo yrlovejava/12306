5e36352e1775f7245001804c8a469a179095fd07
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2025-05-21 15:39:45 +0800

    重构订单表变更后置处理

--- services/ticket-service/src/main/java/com/squirrel/index12306/biz/ticketservice/cannal/OrderCloseCacheAndTokenUpdateHandler.java
+++ services/ticket-service/src/main/java/com/squirrel/index12306/biz/ticketservice/cannal/OrderCloseCacheAndTokenUpdateHandler.java
@@ -1,41 +1,24 @@
 package com.squirrel.index12306.biz.ticketservice.cannal;
 
 import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.util.StrUtil;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONObject;
 import com.squirrel.index12306.biz.ticketservice.common.enums.CanalExecuteStrategyMarkEnum;
-import com.squirrel.index12306.biz.ticketservice.dto.domain.RouteDTO;
-import com.squirrel.index12306.biz.ticketservice.dto.resp.TicketPurchaseRespDTO;
 import com.squirrel.index12306.biz.ticketservice.mq.event.CanalBinlogEvent;
 import com.squirrel.index12306.biz.ticketservice.remote.TicketOrderRemoteService;
 import com.squirrel.index12306.biz.ticketservice.remote.dto.TicketOrderDetailRespDTO;
 import com.squirrel.index12306.biz.ticketservice.remote.dto.TicketOrderPassengerDetailRespDTO;
 import com.squirrel.index12306.biz.ticketservice.service.SeatService;
-import com.squirrel.index12306.biz.ticketservice.service.TrainStationService;
+import com.squirrel.index12306.biz.ticketservice.service.handler.ticket.TicketAvailabilityTokenBucket;
 import com.squirrel.index12306.biz.ticketservice.service.handler.ticket.dto.TrainPurchaseTicketRespDTO;
-import com.squirrel.index12306.framework.starter.bases.Singleton;
-import com.squirrel.index12306.framework.starter.cache.DistributedCache;
-import com.squirrel.index12306.framework.starter.common.toolkit.Assert;
 import com.squirrel.index12306.framework.starter.common.toolkit.BeanUtil;
-import com.squirrel.index12306.framework.starter.convention.exception.ServiceException;
 import com.squirrel.index12306.framework.starter.convention.result.Result;
 import com.squirrel.index12306.framework.starter.designpattern.stategy.AbstractExecuteStrategy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.script.DefaultRedisScript;
-import org.springframework.scripting.support.ResourceScriptSource;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
-
-import static com.squirrel.index12306.biz.ticketservice.common.constant.RedisKeyConstant.TICKET_AVAILABILITY_TOKEN_BUCKET;
 
 /**
  * 订单表变更-订单关闭或取消后置处理组件
@@ -47,12 +30,9 @@ import static com.squirrel.index12306.biz.ticketservice.common.constant.RedisKey
 @RequiredArgsConstructor
 public class OrderCloseCacheAndTokenUpdateHandler implements AbstractExecuteStrategy<CanalBinlogEvent,Void> {
 
-    private final DistributedCache distributedCache;
+    private final TicketAvailabilityTokenBucket ticketAvailabilityTokenBucket;
     private final TicketOrderRemoteService ticketOrderRemoteService;
-    private final TrainStationService trainStationService;
     private final SeatService seatService;
-
-    private static final String LUA_TICKET_AVAILABILITY_ROLLBACK_TOKEN_BUCKET_PATH = "lua/ticket_availability_rollback_token_bucket.lua.lua";
 
     @Override
     public void execute(CanalBinlogEvent message) {
@@ -76,49 +56,8 @@ public class OrderCloseCacheAndTokenUpdateHandler implements AbstractExecuteStra
                 List<TrainPurchaseTicketRespDTO> purchaseRespDTOList = BeanUtil.convert(passengerDetails, TrainPurchaseTicketRespDTO.class);
                 seatService.unlock(trainId,orderDetailResultData.getDeparture(),orderDetailResultData.getArrival(),purchaseRespDTOList);
 
-                // 增加令牌桶中令牌的数量
-                // 单例获取LUA执行脚本
-                DefaultRedisScript<Long> actual = Singleton.get(LUA_TICKET_AVAILABILITY_ROLLBACK_TOKEN_BUCKET_PATH,() -> {
-                    DefaultRedisScript<Long> redisScript = new DefaultRedisScript<>();
-                    redisScript.setResultType(Long.class);
-                    redisScript.setScriptSource(new ResourceScriptSource(new ClassPathResource(LUA_TICKET_AVAILABILITY_ROLLBACK_TOKEN_BUCKET_PATH)));
-                    return redisScript;
-                });
-                Assert.notNull(actual);
-                // 拼接LUA脚本的参数
-                // 获取座位类型对应的数量
-                Map<Integer,Long> seatTypeCountMap = passengerDetails.stream()
-                        .collect(Collectors.groupingBy(TicketOrderPassengerDetailRespDTO::getSeatType,Collectors.counting()));
-                JSONArray seatTypeCountArray = seatTypeCountMap.entrySet().stream()
-                        .map(entry -> {
-                            JSONObject jsonObject = new JSONObject();
-                            jsonObject.put("seatType", String.valueOf(entry.getKey()));
-                            jsonObject.put("count", String.valueOf(entry.getValue()));
-                            return jsonObject;
-                        })
-                        .collect(Collectors.toCollection(JSONArray::new));
-                // 获取redisTemplate
-                StringRedisTemplate stringRedisTemplate = (StringRedisTemplate) distributedCache.getInstance();
-                // 拼接令牌桶key
-                String bucketKey = TICKET_AVAILABILITY_TOKEN_BUCKET + trainId;
-                // 拼接hashKey
-                String hashKey = StrUtil.join("_",orderDetailResultData.getDeparture(),orderDetailResultData.getArrival());
-                // 计算需要扣减的路线（也就是现在需要回滚的路线）
-                List<RouteDTO> routeDTOList = trainStationService.
-                        listTakeoutTrainStationRoute(trainId, orderDetailResultData.getDeparture(), orderDetailResultData.getArrival());
-                // 执行LUA脚本
-                Long result = stringRedisTemplate.execute(
-                        actual,
-                        List.of(bucketKey, hashKey),
-                        JSON.toJSONString(seatTypeCountArray),
-                        JSON.toJSONString(routeDTOList)
-                );
-
-                // 判断结果
-                if(result == null || !Objects.equals(result,0L)){
-                    log.error("回滚列车余票令牌失败，订单信息: {}",JSON.toJSONString(orderDetailResultData));
-                    throw new ServiceException("回滚列车余票失败");
-                }
+                // 回滚列车余票令牌
+                ticketAvailabilityTokenBucket.rollbackInBucket(orderDetailResultData);
             }
         }
     }
